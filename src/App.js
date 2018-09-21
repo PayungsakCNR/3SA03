34f348a7b0f66b026348db5d3183578a5a18c438
PayungsakCNR <payungsak.k@cnr.psu.ac.th>
2018-09-21 23:50:59 +0700

    Update App.js
    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="card">
-        <WordCard  value="hello"/>
+        <prepareStateFromWord  value="Hello"/>
       </div>
     );
   }
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,8 +1,4 @@
-import React, { Component } from 'react';
-import CharacterCard from './CharacterCard';
 import _ from 'lodash';
-
-export default class WordCard extends Component {
         const prepareStateFromWord = (given_word) => {
             let word = given_word.toUpperCase()
             let chars = _.shuffle(Array.from(word))
@@ -14,17 +10,18 @@ export default class WordCard extends Component {
                 completed: false
             }
         }
-        render(){
-        return(
-            <div>
-            { 
-                Array.from(this.props.value).map((c, i) => <CharacterCard value={prepareStateFromWord } key={i}/>)
-            }
-            </div>
-        );
+        const activationHandler = (c) => {
+            let guess = [...this.state.guess, c]
+            this.setState({guess})
+            if(guess.length == this.state.chars.length){
+                if(guess.join('').toString() == this.state.word){
+                    this.setState({guess:[], completed:true})
+                }
+            else{
+                this.setState({guess:[], attempt: this.state.attempt+ 1})
+                }
         }
-        
+        }
 
-}
 
 
